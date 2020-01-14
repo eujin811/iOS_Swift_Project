9313d6176bace82fadaaf72bbbe475a5bdc7eb31
eujin811 <eujin8@gmail.com>
2020-01-14 13:02:13 +0900

    Dynamic AutoLayout

--- 20200110/UiPageViewControllerEx/UiPageViewControllerEx/ViewController.swift
+++ 20200110/UiPageViewControllerEx/UiPageViewControllerEx/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let scrollView = UIScrollView()
+    
+    private let firstView = UIView()
+    private let secondView = UIView()
+    private let thirdView = UIView()
+    private let quartView = UIView()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        attribute()
+        setUpUI()
+    }
+    
+    private func setUpUI() {
+        
+    }
+    private func attribute() {
+        firstView.backgroundColor = .lightGray
+        secondView.backgroundColor = .gray
+        thirdView.backgroundColor = .darkGray
+        quartView.backgroundColor = .black
+        
+        view.addSubview(scrollView)
+        
+        view.addSubview(firstView)
+        view.addSubview(secondView)
+        view.addSubview(thirdView)
+        view.addSubview(quartView)
     }
 
 
---
+++ 20200113Animation/AnmationPlottionButton/AnmationPlottionButton/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AnmationPlottionButton
+//
+//  Created by YoujinMac on 2020/01/13.
+//  Copyright © 2020 YoujinMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        attribute()
+        setUPUI()
+    }
+    
+    private func setUPUI() {
+        
+    }
+    private func attribute() {
+        
+    }
+
+}
+
