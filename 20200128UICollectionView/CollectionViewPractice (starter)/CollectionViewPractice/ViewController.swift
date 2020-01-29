8dcf8a88da704fe777e758bdbe2a2a876c1a8dce
eujin811 <eujin8@gmail.com>
2020-01-29 17:21:51 +0900

    UICollectionView Example Starter

---
+++ 20200128UICollectionView/CollectionViewPractice (starter)/CollectionViewPractice/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CollectionViewPractice
+//
+//  Created by Giftbot on 2020/01/28.
+//  Copyright © 2020 Giftbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  // MARK: LifeCycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: Action
+
+}
+
+
+// MARK: - UICollectionViewDataSource
+
+extension ViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    return UICollectionViewCell()
+  }
+}
