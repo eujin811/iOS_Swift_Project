67e696bad8e0c3fff8219f565c787d4c4816b389
WeatherApp_iOS <eujin8@gmail.com>
2020-06-24 19:37:11 +0900

    locationRelatedFunction

---
+++ locationRelatedFunction/LocationAuthorize/LocationAuthorize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationAuthorize
+//
+//  Created by YoujinMac on 2020/06/22.
+//  Copyright © 2020 YoujinMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
